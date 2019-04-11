4a886cf2f6b632cbcae13bbc17f0202875fe1b43
Andrey Shamis <lolnik@gmail.com>
2019-04-11 19:08:19 +0300

    Improve code
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- main.py
+++ main.py
@@ -9,17 +9,21 @@ import time
 import logging
 import httplib
 import datetime
-from threading import Semaphore
 
 
+URL_TO_CHECK = "https://www.youtube.com/watch?v=O_Nzv-PNMmE"  # type: str
 input_file = 'proxylist.txt'
-threads = 10
+CHECK_YOUTUBE_COUNTRY = True  # type: bool
+THREADS = 10  # number of checker at same time
+USE_LOCK = False  # if True - only one checker will work
+PRINT_BAD = False  # for debug
+PRINT_START = False  # for debug
+ADD_ERROR_LOG = False
+ADD_ALL_LOG = False
 queue = Queue.Queue()
 output = []
-CONTINUE_LOOP = True
-PRINT_BAD = False
-PRINT_START = False
-USE_LOCK = False
+CONTINUE_LOOP = True  # for internal use
+USER_AGENT= 'Mozilla/5.0'  # type: str
 
 run_prefix = datetime.datetime.now().strftime("%y%m%d_%H%M")
 log_frmt = '%(asctime)s | %(name)-10s | %(levelname)-9s | %(message)s'
@@ -35,19 +39,22 @@ formatter = logging.Formatter(log_frmt, datefmt=datefmt)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-# create error file handler and set level to error
-handler = logging.FileHandler(os.path.join("./", "error_{}.log".format(run_prefix)), "w", encoding=None, delay="true")
-handler.setLevel(logging.ERROR)
-formatter = logging.Formatter(log_frmt, datefmt=datefmt)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+if ADD_ERROR_LOG:
+    # create error file handler and set level to error
+    handler = logging.FileHandler(os.path.join("./", "error_{}.log".format(run_prefix)), "w", encoding=None,
+                                  delay="true")
+    handler.setLevel(logging.ERROR)
+    formatter = logging.Formatter(log_frmt, datefmt=datefmt)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
-# create debug file handler and set level to debug
-handler = logging.FileHandler(os.path.join("./", "all_{}.log".format(run_prefix)), "w")
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter(log_frmt, datefmt=datefmt)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+if ADD_ALL_LOG:
+    # create debug file handler and set level to debug
+    handler = logging.FileHandler(os.path.join("./", "all_{}.log".format(run_prefix)), "w")
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(log_frmt, datefmt=datefmt)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
 loaded = False
 logging.info("Starting execution : ID {}".format(run_prefix))
@@ -64,7 +71,6 @@ class ThreadUrl(threading.Thread):
 
     def run(self):
         while CONTINUE_LOOP:
-            #grabs host from queue
             proxy_info = clean_proxy_info = first_input = ''
             ThreadUrl.counter += 1
             try:
@@ -83,9 +89,9 @@ class ThreadUrl(threading.Thread):
                         logging.info('Start {}'.format(proxy_info))
                     proxy_handler = urllib2.ProxyHandler({'https': proxy_info})
                     opener = urllib2.build_opener(proxy_handler)
-                    opener.addheaders = [('User-agent','Mozilla/5.0')]
+                    opener.addheaders = [('User-agent', USER_AGENT)]
                     urllib2.install_opener(opener)
-                    req = urllib2.Request("https://www.youtube.com/watch?v=O_Nzv-PNMmE")
+                    req = urllib2.Request(URL_TO_CHECK)
                     if USE_LOCK:
                         ThreadUrl.connect_sem.acquire()
                     try:
@@ -100,17 +106,17 @@ class ThreadUrl(threading.Thread):
                             ThreadUrl.connect_sem.release()
                     #  and '<title>YouTube</title>' in rs
                     t_r = round(end_t-start_t, 3)
-                    if sock.msg == 'OK' and sock.code == 200 and '<title>' in rs and 'UNPLAYABLE' not in rs:
-                        logging.info('{:>3} Success [ {:>21} ] ( in {} )'.format(ThreadUrl.counter,
-                            proxy_info, datetime.timedelta(seconds=t_r)))
+                    if sock.msg == 'OK' and sock.code == 200 and '<title>' in rs \
+                            and (CHECK_YOUTUBE_COUNTRY and 'UNPLAYABLE' not in rs):
+                        logging.info('{:>3} Success [ {:>21} ] ( in {} )'.format(
+                            ThreadUrl.counter, proxy_info, datetime.timedelta(seconds=t_r)))
                         output.append((t_r, proxy_info))
                     else:
                         if PRINT_BAD:
                             if sock.msg != 'OK' or sock.code != 200:
                                 logging.error('BAD socket')
-                            if 'UNPLAYABLE' in rs:
+                            if CHECK_YOUTUBE_COUNTRY and 'UNPLAYABLE' in rs:
                                 logging.error('UNPLAYABLE FOUND')
-                        #raise Exception("{} Not Google".format(clean_proxy_info))
                 else:
                     logging.debug('Skip connection to [{}]'.format(first_input))
             except (urllib2.URLError, httplib.BadStatusLine) as ex:
@@ -121,28 +127,28 @@ class ThreadUrl(threading.Thread):
                 logging.exception(ex)
                 logging.error("------------------")
                 # output.append(('x', proxy_info))
-            #signals to queue job is done
-            self.queue.task_done()
+            self.queue.task_done()  # signals to queue job is done
 
 
 def signal_handler(sig, frame):
     global CONTINUE_LOOP
     CONTINUE_LOOP = False
     print('INTERRUPT: Exiting from MAIN {}'.format(sig))
+    time.sleep(0.5)
     sys.exit(27)
 
 
 def main():
-    #spawn a pool of threads, and pass them queue instance
-    for i in range(5):
+    # spawn a pool of threads, and pass them queue instance
+    for i in range(THREADS):
         t = ThreadUrl(queue)
         t.setDaemon(True)
         t.start()
-    hosts = [host.strip() for host in open(input_file).readlines()]
-    #populate queue with data
-    for host in hosts:
-        queue.put(host)
-    #wait on the queue until everything has been processed
+    hosts = [host_tmp.strip() for host_tmp in open(input_file).readlines()]
+    # populate queue with data
+    for host_tmp in hosts:
+        queue.put(host_tmp)
+    # wait on the queue until everything has been processed
     queue.join()
 
 
